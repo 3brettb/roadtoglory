f2c0bfcbab0e52241a5da4d851bc537fe49a9601
3brettb <3brettb@gmail.com>
2017-05-09 16:12:47 -0600

    Version 1 Stable 1

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 1.0.0
+        <!--Version.Stable.Bug -->
+        <b>Version</b> 1.1.0
     </div>
     <strong>Copyright &copy; 2017 <a href="http://brettbrist.blackfiddle.net">Brett Brist</a> | RoadToGlory.</strong> All rights reserved.
 </footer>
