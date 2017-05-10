85de6f47b372f035bd82a456e67c4e112bf72546
3brettb <3brettb@gmail.com>
2017-05-10 15:32:48 -0600

    Initial Player Table

--- resources/views/players/index.blade.php
+++ resources/views/players/index.blade.php
@@ -13,39 +13,45 @@
 @endsection
 
 @section('content')
-    <table id="players-table" class="table table-bordered">
-        <thead>
-            <tr>
-                <th>Firstname</th>
-                <th>Lastname</th>
-            </tr>
-        </thead>
-        <tbody></tbody>
-    </table>
+    <section class="content">
+        <table id="table"></table>
+    </section>
 @endsection
 
+@push('bottom-script-list')
+    <script src="{{ URL::asset('plugins/bootstrap-table/src/bootstrap-table.js') }}"></script>
+@endpush
+
 @push('bottom-scripts')
-    $(document).ready(function(){
-        $("#players-table").datatable({
-            pageSize: 50,
-            sort: [true],
-            filters: [true],
-            filterText: 'Type to filter...',
-            data: [
-                @foreach($players as $player)
-                    ['{{$player->firstname}}', '{{$player->lastname}}'],
-                @endforeach
-            ]
-        });
+    $('#table').bootstrapTable({
+        columns: [{
+            field: 'firstname',
+            title: 'First'
+        }, {
+            field: 'lastname',
+            title: 'Last'
+        }, {
+            field: 'position',
+            title: 'POS'
+        }, {
+            field: 'teamAbbr',
+            title: 'NFL'
+        }, {
+            field: 'owner',
+            title: 'Team'
+        }, {
+            field: 'status',
+            title: 'Status'
+        }],
+        data: [
+            @foreach($players as $player)
+                <?php echo json_encode($player); ?>,
+            @endforeach
+        ]
     });
 @endpush
 
 @section('head-script')
-    <!-- If you are using bootstrap: -->
-    <link rel="stylesheet" type="text/css" href="{{ URL::asset('plugins/datatable/css/datatable-bootstrap.min.css') }}" media="screen">
-                            
-    <!-- JS files -->
-    <script type="text/javascript" src="{{ URL::asset('plugins/datatable/js/datatable.min.js') }}"></script>
-    <script type="text/javascript" src="{{ URL::asset('plugins/datatable/js/datatable.jquery.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ URL::asset('plugins/bootstrap-table/src/bootstrap-table.css') }}">
 @endsection
 
